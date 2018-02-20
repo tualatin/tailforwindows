22c916ee09cced1c98a988922e70006a97193e8e
Markus Zönnchen <Markus.Zoennchen@dmtm.com>
2018-02-20 15:37:25 +0100

    Add proxy settings page

---
+++ Tail4Windows/UI/Converters/InverseBooleanConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Data;
+
+
+namespace Org.Vs.TailForWin.UI.Converters
+{
+  /// <summary>
+  /// InverseBooleanConverter
+  /// </summary>
+  public class InverseBooleanConverter : IValueConverter
+  {
+    /// <summary>
+    /// Convert
+    /// </summary>
+    /// <param name="value">Value to convert</param>
+    /// <param name="targetType">TargetType</param>
+    /// <param name="parameter">Parameter</param>
+    /// <param name="culture">Current culture</param>
+    /// <returns>Converted object</returns>
+    public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) => value != null && !(bool) value;
+
+    /// <summary>
+    /// Convert back
+    /// </summary>
+    /// <param name="value">Value to convert back</param>
+    /// <param name="targetType">TargtType</param>
+    /// <param name="parameter">Parameter</param>
+    /// <param name="culture">Current culture</param>
+    /// <returns>Converted object</returns>
+    public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) => value != null && !(bool) value;
+  }
+}
---
+++ Tail4Windows/UI/Converters/StringToIntConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Data;
+
+
+namespace Org.Vs.TailForWin.UI.Converters
+{
+  /// <summary>
+  /// StringToIntConverter
+  /// </summary>
+  public class StringToIntConverter : IValueConverter
+  {
+    /// <summary>
+    /// Convert
+    /// </summary>
+    /// <param name="value">Value</param>
+    /// <param name="targetType">Target type</param>
+    /// <param name="parameter">Parameter</param>
+    /// <param name="culture">Culture</param>
+    /// <returns>Converted value</returns>
+    public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+      if ( !(value is int port) )
+        return null;
+
+      if ( port < 0 )
+        port = 0;
+
+      return port;
+    }
+
+    /// <summary>
+    /// Convert back
+    /// </summary>
+    /// <param name="value">Value</param>
+    /// <param name="targetType">Target type</param>
+    /// <param name="parameter">Parameter</param>
+    /// <param name="culture">Culture</param>
+    /// <returns>Converted value</returns>
+    public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
