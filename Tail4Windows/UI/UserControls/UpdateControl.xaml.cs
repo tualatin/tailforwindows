1e82667de8d79f2ec3a1c9c0799f776326fd9f30
Markus Zönnchen <Markus.Zoennchen@dmtm.com>
2018-03-06 09:35:18 +0100

    Working on autoupdate

--- Tail4Windows/UI/UserControls/UpdateControl.xaml.cs
+++ Tail4Windows/UI/UserControls/UpdateControl.xaml.cs
@@ -1,28 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace Org.Vs.TailForWin.UI.UserControls
+﻿namespace Org.Vs.TailForWin.UI.UserControls
 {
   /// <summary>
   /// Interaction logic for UpdateControl.xaml
   /// </summary>
-  public partial class UpdateControl : UserControl
+  public partial class UpdateControl
   {
-    public UpdateControl()
-    {
-      InitializeComponent();
-    }
+    /// <summary>
+    /// Standard constructor
+    /// </summary>
+    public UpdateControl() => InitializeComponent();
   }
 }
---
+++ Tail4Windows/UI/ViewModels/AutoUpdateViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Org.Vs.TailForWin.Core.Data.Base;
+using Org.Vs.TailForWin.UI.UserControls.Interfaces;
+
+
+namespace Org.Vs.TailForWin.UI.ViewModels
+{
+  /// <summary>
+  /// Auto update view model
+  /// </summary>
+  public class AutoUpdateViewModel : NotifyMaster, IUpdateControlViewModel
+  {
+    public string ApplicationVersion
+    {
+      get => throw new NotImplementedException();
+      set => throw new NotImplementedException();
+    }
+    public string WebVersion
+    {
+      get => throw new NotImplementedException();
+      set => throw new NotImplementedException();
+    }
+    public string UpdateHint
+    {
+      get => throw new NotImplementedException();
+      set => throw new NotImplementedException();
+    }
+  }
+}
