ee37b25884e65c473a702471c22a54269cb52e19
Markus Zönnchen <Markus.Zoennchen@dmtm.com>
2018-03-06 09:42:10 +0100

    AutpUpdatePopUp is WindowEx now

--- Tail4Windows/UI/ViewModels/AutoUpdateViewModel.cs
+++ Tail4Windows/UI/ViewModels/AutoUpdateViewModel.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Org.Vs.TailForWin.Core.Data.Base;
+﻿using Org.Vs.TailForWin.Core.Data.Base;
 using Org.Vs.TailForWin.UI.UserControls.Interfaces;
 
 
@@ -10,20 +9,58 @@ namespace Org.Vs.TailForWin.UI.ViewModels
   /// </summary>
   public class AutoUpdateViewModel : NotifyMaster, IUpdateControlViewModel
   {
+    private string _applicationVersion;
+
+    /// <summary>
+    /// Current application version
+    /// </summary>
     public string ApplicationVersion
     {
-      get => throw new NotImplementedException();
-      set => throw new NotImplementedException();
+      get => _applicationVersion;
+      set
+      {
+        if ( Equals(_applicationVersion, value) )
+          return;
+
+        _applicationVersion = value;
+        OnPropertyChanged(nameof(ApplicationVersion));
+      }
     }
+
+    private string _webVersion;
+
+    /// <summary>
+    /// Current web version
+    /// </summary>
     public string WebVersion
     {
-      get => throw new NotImplementedException();
-      set => throw new NotImplementedException();
+      get => _webVersion;
+      set
+      {
+        if ( Equals(_webVersion, value) )
+          return;
+
+        _webVersion = value;
+        OnPropertyChanged(nameof(WebVersion));
+      }
     }
+
+    private string _updateHint;
+
+    /// <summary>
+    /// Update hint text
+    /// </summary>
     public string UpdateHint
     {
-      get => throw new NotImplementedException();
-      set => throw new NotImplementedException();
+      get => _updateHint;
+      set
+      {
+        if ( Equals(_updateHint, value) )
+          return;
+
+        _updateHint = value;
+        OnPropertyChanged(nameof(UpdateHint));
+      }
     }
   }
 }
