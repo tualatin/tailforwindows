c2053f1cd065aa348c6cf7ef30257ba85eea8653
tualatin <development@virtual-studios.org>
2018-06-20 10:30:51 +0200

    Set version informations of business

---
+++ Tail4WindowsBusiness/StatisticEngine/Controllers/StatisticController.cs
@@ -0,0 +1,13 @@
+﻿using Org.Vs.TailForWin.Business.StatisticEngine.Interfaces;
+
+
+namespace Org.Vs.TailForWin.Business.StatisticEngine.Controllers
+{
+  /// <summary>
+  /// Statistics controller
+  /// </summary>
+  public class StatisticController : IStatisticController
+  {
+
+  }
+}
---
+++ Tail4WindowsBusiness/StatisticEngine/Interfaces/IStatisticController.cs
@@ -0,0 +1,10 @@
+﻿namespace Org.Vs.TailForWin.Business.StatisticEngine.Interfaces
+{
+  /// <summary>
+  /// Statistics controller interface
+  /// </summary>
+  public interface IStatisticController
+  {
+
+  }
+}
