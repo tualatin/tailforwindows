2df42e5f612346b5932364a96545a6d950691b58
Markus Zönnchen <Markus.Zoennchen@dmtm.org>
2023-01-02 13:31:21 +0100

    Update nuget packages

--- Tail4Windows/Properties/AssemblyInfo.cs
+++ Tail4Windows/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Markup;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -52,10 +53,10 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.3.8382")]
-[assembly: AssemblyFileVersion("2.3.8382")]
+[assembly: AssemblyVersion("2.3.8402")]
+[assembly: AssemblyFileVersion("2.3.8402")]
 [assembly: log4net.Config.XmlConfigurator(ConfigFile = "Config/log4net.xml", Watch = true)]
 [assembly: Guid("c2612b08-dbd5-4002-a1f0-e1d789a906e7")]
 
 
-[assembly: AssemblyInformationalVersion("2.3.8382.3")]
+[assembly: AssemblyInformationalVersion("2.3.8402.4")]
--- Tail4WindowsUi.Plugins/Properties/AssemblyInfo.cs
+++ Tail4WindowsUi.Plugins/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.21")]
-[assembly: AssemblyFileVersion("1.0.21")]
+[assembly: AssemblyVersion("1.0.22")]
+[assembly: AssemblyFileVersion("1.0.22")]
 [assembly: log4net.Config.XmlConfigurator(ConfigFile = "Config/log4net.xml", Watch = true)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
